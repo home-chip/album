1c20302d648f659f68fe42f10f58299bf17c8d2c
grampaer <grampaer@gmail.com>
2018-12-12 05:37:43 +0100

    Add list files

--- index.php
+++ index.php
@@ -2,6 +2,32 @@
 
 <html>
 
+<?php
+
+function list_files($dir)
+{
+    if ($handle = opendir($dir)) {
+ 		echo "Directory : $dir\n";
+		while (false !== ($entry = readdir($handle))) {
+
+	        if ($entry != "." && $entry != "..") {
+	        	 if (list_files($dir."/".$entry)) {
+	        	 }
+	        	 else {
+		 		     echo "	file : $entry\n";
+	        	 }
+	        }
+	    }
+
+	    closedir($handle);
+		return true;
+	}
+	return false; 
+}
+
+?>
+
+
 <head>
 	<title>Site Web</title>
 </head>
@@ -12,7 +38,7 @@ Voici la liste des photos trouvées sur le nas :
 
 <?php
 
-
+	
 	if ($handle = opendir('/media')) {
 
     	while (false !== ($entry = readdir($handle))) {
